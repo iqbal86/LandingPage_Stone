8bce7b22e6bdd12a38403d627f69a41efa08fc17
Sofiia Romah <sofija.romah@gmail.com>
2020-07-09 20:38:18 +0300

    Created readme file and navbar

---
+++ README.md
@@ -0,0 +1,29 @@
+## Table of contents
+
+- [General info](#general-info)
+- [Technologies](#technologies)
+- [Authors](#authors)
+
+## General info
+
+This project is a promo landing page for stone shop, named ArtStone.
+
+## Sections
+
+- General (main section of a website where visitors can see main units of shope)
+- About (short information about the agency and a little of statistics)
+- Benefits
+- Gallery (photos of works)
+- Contact Us (Form to ask question about everything you want to know) and Contact Info
+
+## Technologies
+
+Project is created with:
+
+- HTML5
+- CSS3
+- Code editor(I worked in Visual Studio Code)
+
+## Authors
+
+Created by Sofiia Romakh
---
+++ index.js
@@ -0,0 +1,49 @@
+var image;
+
+window.onload = function () {
+  image = document.getElementById("clickAndChange");
+
+  //   Immediately invoked function to set the theme on initial load
+  (function () {
+    if (localStorage.getItem("theme") === "theme-dark") {
+      setTheme("theme-dark");
+      image.src = "images/dark.png";
+      image.classList.add("invert-image");
+    } else {
+      setTheme("theme-light");
+      image.src = "images/light.png";
+      image.classList.remove("invert-image");
+    }
+  })();
+};
+
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+  localStorage.setItem("theme", themeName);
+  document.documentElement.className = themeName;
+}
+// function to toggle between light and dark theme
+function toggleTheme() {
+  if (localStorage.getItem("theme") === "theme-dark") {
+    setTheme("theme-light");
+    image.src = "images/light.png";
+    image.classList.remove("invert-image");
+  } else {
+    setTheme("theme-dark");
+    image.src = "images/dark.png";
+    image.classList.add("invert-image");
+  }
+}
+
+function toggleNavbar() {
+  var navbar = document.getElementsByClassName("navbar");
+  navbar[0].classList.toggle("toggled");
+
+  var navMenu = document.getElementsByClassName("nav-menu")[0];
+  var navHeight = navMenu.scrollHeight;
+  if (navMenu.style.height == navHeight + "px") {
+    navMenu.style.height = "0px";
+  } else {
+    navMenu.style.height = navHeight + "px";
+  }
+}
